bc4db20b8c2c7b50b2cdd166758be2dec85d4692
kruzikh <kruzik@gnomon.cz>
2021-06-30 19:23:18 +0200

    New README and updated script
    README added, script modified to hane order of elements as required.

--- source-update/update-rat.sh
+++ source-update/update-rat.sh
@@ -8,8 +8,8 @@ curl --silent --show-error https://covid-19-diagnostics.jrc.ec.europa.eu/devices
 ($i.hsc_list_history | sort_by(.list_date)[-1]) as $latest |
 {
   "display": ($i.manufacturer.name + ", " + $i.commercial_name),
-  "system": "https://covid-19-diagnostics.jrc.ec.europa.eu/devices",
-  "version":  (if $latest.in_common_list then $i.last_updated else $latest.list_date end),
   "lang": "en",
-  "active": $latest.in_common_list }))
+  "active": $latest.in_common_list,
+  "system": "https://covid-19-diagnostics.jrc.ec.europa.eu/devices",
+  "version":  (if $latest.in_common_list then $i.last_updated else $latest.list_date end) }))
 }'
